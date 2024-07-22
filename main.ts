f3d3bb38de05495cd8d0460f99673c8eb272e2f3
MarceloSestrem <145679834+MarceloSestrem@users.noreply.github.com>
2024-07-22 10:10:37 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -306,6 +306,41 @@ namespace Seguidor_de_Linha {
         medianRoundTrip: number;
         travelTimeObservers: number[];
     }
+     /**
+     * Acquiring ultrasonic data
+     * @param trig trig pin selection enumeration, eg:DigitalPin.P13
+     * @param echo echo pin selection enumeration, eg:DigitalPin.P14
+     */
+//% group="Ultrassônico"
+    //% blockId="labcode_ultrasonico_conectado"
+    //% block="Sensor Ultrassônico pino TRIG %trig Pino ECHO %echo Distância unit:cm"
+    //% weight=94
+    export function readUltrasonic(trig:DigitalPin, echo:DigitalPin):number{
+        let data;
+        pins.digitalWritePin(trig, 1);
+        basic.pause(1);
+        pins.digitalWritePin(trig, 0)
+        if(pins.digitalReadPin(echo) == 0){
+            pins.digitalWritePin(trig, 0);
+            pins.digitalWritePin(trig, 1);
+            basic.pause(20);
+            pins.digitalWritePin(trig, 0);
+            data = pins.pulseIn(echo, PulseValue.High,500*58);
+        }else{
+            pins.digitalWritePin(trig, 1);
+            pins.digitalWritePin(trig, 0);
+            basic.pause(20);
+            pins.digitalWritePin(trig, 0);
+            data = pins.pulseIn(echo, PulseValue.High,500*58)
+        }
+        data = data / 59;
+        if(data <= 0)
+            return 0;
+        if(data > 500)
+            return 500;
+        return Math.round(data);
+    }
+
 
     let ultrasonicState: UltrasonicDevice;
 
