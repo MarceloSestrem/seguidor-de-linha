15b16a25dcd616e6c67f3657f0a706230f018940
MarceloSestrem <145679834+MarceloSestrem@users.noreply.github.com>
2024-07-22 11:07:12 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -316,8 +316,8 @@ namespace Seguidor_de_Linha {
         * @param trig trig pin selection enumeration, eg:DigitalPin.P13
         * @param echo echo pin selection enumeration, eg:DigitalPin.P14
         * @param unit unit of distance, eg: DistanceUnit.CM
-        */
-//% group="Ultrassônico"
+        
+    //% group="Ultrassônico"
     //% blockId="labcode_ultrasonico_conectado"
     //% block="Sensor Ultrassônico pino TRIG %trig pino ECHO %echo em %unit"
     //% weight=94
