3da24c496eb0b079b0a5073d972e6f65731c48fa
MarceloSestrem <145679834+MarceloSestrem@users.noreply.github.com>
2024-07-27 03:10:55 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -715,7 +715,7 @@ namespace Seguidor_de_Linha {
     }
 
     //% weight=79
-    //% blockId="tresssensoresanalog" block="Detecção dos sensores de linha Analógica %Tressensores| Média= %m"
+    //% blockId="tresssensoresanalog" block="Detecção dos sensores de linha Analógica %Tressensores| Média = %m"
     //% group="Sensores de linha V.2( pinos: P0,  P1 e ou  P2 Cores: Branco: □ e Preto: ■)"
     //% m.min=0 m.max=1023
     export function readtresa(tresa: Tressensores, m: number): boolean {
@@ -766,7 +766,7 @@ namespace Seguidor_de_Linha {
 
 
     //% weight=79
-    //% blockId="doissensoresa" block="Detecção dos sensores de linha Analógica %Doissensores | Média= %m"
+    //% blockId="doissensoresa" block="Detecção dos sensores de linha Analógica %Doissensores | Média = %m"
    //% group="Sensores de linha V.2( pinos: P0,  P1 e ou  P2 Cores: Branco: □ e Preto: ■)"
     //% m.min=0 m.max=1023
     export function readDoisa(doisa: Doissensores, m: number): boolean {
