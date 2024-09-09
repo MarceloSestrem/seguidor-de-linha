46787588af31b87451eba6dc0edad62f400953cc
MarceloSestrem <145679834+MarceloSestrem@users.noreply.github.com>
2024-09-08 23:13:23 -0300

    Atualizar 'pxt.json, main.ts'

--- main.ts
+++ main.ts
@@ -316,7 +316,7 @@ namespace Seguidor_de_Linha {
     //% group="Motores" weight=58
     //% speed1.min=-255 speed1.max=255
     //% speed2.min=-255 speed2.max=255
-        //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
+        //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
     export function MotorRunDualDELAY(motor1: Motors, speed1: number, motor2: Motors, speed2: number, delay: number): void {
         MotorRun(motor1, speed1);
         MotorRun(motor2, speed2);
